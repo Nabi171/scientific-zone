321c0f262158667b74193812353ffbf9457d990d
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-04-18 14:03:03 +0600

    sign up from updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>this is about page</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-5'>
+                    Name:Md Nabiul
+          
+          </div>
+                <div className='col-5'>
+                    Future Goal: <br />
+                    My goal is to be a complete web developer.I have worked to reach my goal,basically i am i Chittagong University in the department of mathematics.I wanted to do something beside my study,so be a web developer will create a another identity,Thats why i used to make coding daily 8 to 10 hours in my daily routine to acheive my goal.That was quite difficulties to come this point to make code from joint family, that day i never forget when my mother got hospitalized but i had make continue my coding with handling my mother situation.But by the almighty allah,i successfully handle the both situation.Thats all from me.Keep me in your prayers as i can achieve my goal.
+           </div>
+            </div>
         </div>
     );
 };
