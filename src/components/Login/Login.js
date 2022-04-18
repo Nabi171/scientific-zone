db982de25f566c121c58a01ee2160ab0d3d4556c
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-04-18 17:18:53 +0600

    error handleing with signup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,14 +5,21 @@ import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 import OthersLogin from '../Others/OthersLogin';
+import { AiOutlineExclamationCircle } from "react-icons/ai";
+import Loading from '../Loading/Loading';
 
 
 
 
 const Login = () => {
+    const [emailError, setEmailError] = useState();
+    const [passwordError, setPasswordError] = useState();
+
     const [
         signInWithEmailAndPassword,
-        user
+        user,
+        loading,
+        error,
     ] = useSignInWithEmailAndPassword(auth);
 
     const [email, SetEmail] = useState();
@@ -24,12 +31,25 @@ const Login = () => {
     if (user) {
         navigate('/home')
     }
+    if (loading) {
+        <Loading />
+    }
+
     const handlePassword = e => {
         SetPassword(e.target.value);
     }
 
     const handleUser = async (e) => {
         e.preventDefault();
+        if (!email) {
+            setEmailError('Please Put a Email');
+        }
+        if (!password) {
+            setPasswordError('Please Put a password');
+        }
+        if (!email || !password) {
+            return;
+        }
         await signInWithEmailAndPassword(email, password);
     }
 
@@ -41,13 +61,17 @@ const Login = () => {
                 <div className="inputs-form">
                     <label className='form-label'>EMAIL</label>
                     <input onBlur={handleEmail} className="form-input" type="email" placeholder="example@test.com" />
+                    {emailError && <p className='text-danger mt-2'><AiOutlineExclamationCircle className='mb-1' /> {emailError}</p>}
+
                     <label className='form-label'>PASSWORD</label>
                     <input onBlur={handlePassword} className="form-input" type="password" placeholder="Min 6 charaters long" />
+                    {passwordError && <p className='text-danger mt-2'><AiOutlineExclamationCircle className='mb-1' /> {passwordError}</p>}
                     <br />
                     <button className='form-btn' type="submit">LOGIN</button>
                 </div>
 
             </form>
+            {error && <p className='text-danger mb-0 mt-3'>{error.message}</p>}
             <br />
             <p>You are new user!please signup. <Link to='/signup' className='text-danger pe-auto text-decoration-none fw-bold'>Please SignUp</Link></p>
             <br />
