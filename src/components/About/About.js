a0205021f9cac9ee66534aca8fe190eb2b5d4163
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-04-18 14:08:13 +0600

    about section added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,10 @@ const About = () => {
             <div className='row'>
                 <div className='col-5'>
                     Name:Md Nabiul
-          
+
           </div>
-                <div className='col-5'>
-                    Future Goal: <br />
+                <div className='col-5 border shadow p-5'>
+                    <span className='fw-bolder text-danger'>FUTURE GOAL</span><br /> <hr />
                     My goal is to be a complete web developer.I have worked to reach my goal,basically i am i Chittagong University in the department of mathematics.I wanted to do something beside my study,so be a web developer will create a another identity,Thats why i used to make coding daily 8 to 10 hours in my daily routine to acheive my goal.That was quite difficulties to come this point to make code from joint family, that day i never forget when my mother got hospitalized but i had make continue my coding with handling my mother situation.But by the almighty allah,i successfully handle the both situation.Thats all from me.Keep me in your prayers as i can achieve my goal.
            </div>
             </div>
