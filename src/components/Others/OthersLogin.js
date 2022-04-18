6a72d1abe2a5469b43310dfc4abb4817b8126174
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-04-18 18:17:50 +0600

    updating social

--- src/components/Others/OthersLogin.js
+++ src/components/Others/OthersLogin.js
@@ -4,9 +4,24 @@ import github from '../Others/github.png'
 import { Card as div } from 'react-bootstrap';
 import auth from '../../firebase.init';
 import { useSignInWithGoogle, useSignInWithGithub } from 'react-firebase-hooks/auth';
+import Loading from '../Loading/Loading';
+import { useNavigate } from 'react-router';
 const OthersLogin = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+    const navigate = useNavigate();
+    let errorhandleing;
+    if (error || error1) {
+        errorhandleing =
+            <p className='text-danger'>Error: {error ?.message} {error1 ?.message}</p>
+    }
+    if (loading || loading1) {
+        return <Loading></Loading>
+    }
+
+    if (user || user1) {
+        navigate('/home')
+    }
     return (
         <div>
             <div className='mt-4'>
@@ -22,6 +37,7 @@ const OthersLogin = () => {
                         className='btn btn-info d-block mx-auto w-50 my-2'>
                         <img style={{ width: '30px' }} className='me-4' src={goggle} alt="" /> <span className='px-2'>Goggle Sign In</span></button>
                 </div>
+                {errorhandleing}
             </div>
         </div>
     );
